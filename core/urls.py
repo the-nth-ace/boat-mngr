85cfa8251f5e09b38810f8a59a59a779ddf1e79b
Olatubosun Femi <thefemiolatubosun@gmail.com>
2022-07-23 10:19:59 +0100

    Basic Admin template

--- core/urls.py
+++ core/urls.py
@@ -12,9 +12,13 @@ urlpatterns = [
 
 
 # Owner Dashboard Views
-
 urlpatterns += [path("owner/", owner_dashboard.dashboard, name="owner_dashboard")]
 
 # Admin Dashboard Views
 # NOTE The url is '/appadmin so as not to conflict with Django's default '/admin'
-urlpatterns += [path("appadmin/", admin_dashboard.dashboard, name="admin_dashboard")]
+urlpatterns += [
+    path("appadmin/", admin_dashboard.dashboard, name="admin_dashboard"),
+    path("appadmin/owners/", admin_dashboard.all_owners, name="admin_all_owners"),
+    path("appadmin/boats/", admin_dashboard.all_boats, name="admin_all_boats"),
+    path("appadmin/reviews/", admin_dashboard.all_reviews, name="admin_all_reviews"),
+]
--- core/views/admin_dashboard.py
+++ core/views/admin_dashboard.py
@@ -12,7 +12,20 @@ from core.services import is_admin_test
 
 @user_passes_test(is_admin_test)
 def dashboard(request):
-    return render(request, "admin/dashboard.html")
+
+    return render(request, "admin/dashboard.html", context={"active": "dashboard"})
 
 
-#
+@user_passes_test(is_admin_test)
+def all_owners(request):
+    return render(request, "admin/all_owners.html", context={"active": "businesses"})
+
+
+@user_passes_test(is_admin_test)
+def all_boats(request):
+    return render(request, "admin/all_boats.html", context={"active": "boats"})
+
+
+@user_passes_test(is_admin_test)
+def all_reviews(request):
+    return render(request, "admin/all_reviews.html", context={"active": "reviews"})
