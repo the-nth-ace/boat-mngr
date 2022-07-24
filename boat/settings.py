51d91ce4e2cdc6c5ab24081e98d19fc5a6a25278
Olatubosun Femi <thefemiolatubosun@gmail.com>
2022-07-24 20:17:29 +0100

    Static files

--- boat/settings.py
+++ boat/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIR = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIR = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
