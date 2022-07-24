494618aee6d582d701ae344a6dd34c6ad820e221
Olatubosun Femi <thefemiolatubosun@gmail.com>
2022-07-24 19:55:51 +0100

    First dev steps

--- boat/settings.py
+++ boat/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-v@x=_f**j5*%zhkq1fvl#^phb@-2hx554t^-79!zhb56$1jfzc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
