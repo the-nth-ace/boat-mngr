3261e77e12360da14f56301334f11a345899d874
Olatubosun Femi <thefemiolatubosun@gmail.com>
2022-07-24 21:47:56 +0100

    Database setup

--- boat/settings.py
+++ boat/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,6 +88,8 @@ DATABASES = {
     }
 }
 
+
+DATABASES["default"] = dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
