6c6a763e6035cfbc64b1b020771277b6f2a4f8cd
Olatubosun Femi <thefemiolatubosun@gmail.com>
2022-07-24 03:33:54 +0100

    Basic CRUD for business and Boat

--- core/urls.py
+++ core/urls.py
@@ -11,23 +11,66 @@ urlpatterns = [
 ]
 
 
-# Owner Dashboard Views
-urlpatterns += [path("owner/", owner_dashboard.dashboard, name="owner_dashboard")]
+# Dashboard Views
+## ! Businesses
+urlpatterns += [
+    path(
+        "dashboard/businesses",
+        admin_dashboard.BusinessListView.as_view(),
+        name="dashboard_business_list",
+    ),
+    path(
+        "dashboard/businesses/add",
+        admin_dashboard.BusinessCreateView.as_view(),
+        name="dashboard_business_add",
+    ),
+    path(
+        "dashboard/businesses/update/<int:pk>/",
+        admin_dashboard.BusinessUpdateView.as_view(),
+        name="dashboard_business_update",
+    ),
+    path(
+        "dashboard/businesses/<int:pk>/delete",
+        admin_dashboard.delete_business,
+        name="dashboard_business_delete",
+    ),
+    path(
+        "dashboard/businesses/<int:pk>",
+        admin_dashboard.BusinessDetailView.as_view(),
+        name="dashboard_business_detail",
+    ),
+]
+
+
+# ! Boats
+urlpatterns += [
+    path(
+        "dashboard/boats",
+        admin_dashboard.BoatListView.as_view(),
+        name="dashboard_boat_list",
+    ),
+    path(
+        "dashboard/boats/add/",
+        admin_dashboard.BoatCreateView.as_view(),
+        name="dashboard_boat_add",
+    ),
+    path(
+        "dashboard/boats/<int:pk>/",
+        admin_dashboard.BoatDetailView.as_view(),
+        name="dashboard_boat_detail",
+    ),
+    path(
+        "dashboard/boats/<int:pk>/delete",
+        admin_dashboard.delete_business,
+        name="dashboard_boat_delete",
+    ),
+]
+
 
 # Admin Dashboard Views
 # NOTE The url is '/appadmin so as not to conflict with Django's default '/admin'
 urlpatterns += [
     path("appadmin/", admin_dashboard.dashboard, name="admin_dashboard"),
-    path(
-        "appadmin/businesses/",
-        admin_dashboard.all_businesses,
-        name="admin_all_businesses",
-    ),
-    path(
-        "appadmin/businesses/add",
-        admin_dashboard.add_businesses,
-        name="admin_add_business",
-    ),
     path("appadmin/boats/", admin_dashboard.all_boats, name="admin_all_boats"),
     path("appadmin/boats/add", admin_dashboard.add_boat, name="admin_add_boat"),
     path("appadmin/reviews/", admin_dashboard.all_reviews, name="admin_all_reviews"),
--- core/views/admin_dashboard.py
+++ core/views/admin_dashboard.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from typing import Any, Dict, Optional
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import user_passes_test
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from core.forms import BoatForm
+from core.models import Boat, Business
+
 from core.services import is_admin_test
 
 # Create your views here.
@@ -13,29 +19,140 @@ from core.services import is_admin_test
 @user_passes_test(is_admin_test)
 def dashboard(request):
 
-    return render(request, "admin/dashboard.html", context={"active": "dashboard"})
-
-
-@user_passes_test(is_admin_test)
-def all_businesses(request):
-    return render(request, "admin/all_owners.html", context={"active": "businesses"})
-
-
-@user_passes_test(is_admin_test)
-def add_businesses(request):
-    return render(request, "admin/business_form.html", context={"active": "businesses"})
+    return render(request, "dashboard/dashboard.html", context={"active": "dashboard"})
 
 
 @user_passes_test(is_admin_test)
 def all_boats(request):
-    return render(request, "admin/all_boats.html", context={"active": "boats"})
+    return render(request, "dashboard/all_boats.html", context={"active": "boats"})
 
 
 @user_passes_test(is_admin_test)
 def add_boat(request):
-    return render(request, "admin/boat_form.html", context={"active": "boats"})
+    return render(request, "dashboard/boat_form.html", context={"active": "boats"})
 
 
 @user_passes_test(is_admin_test)
 def all_reviews(request):
-    return render(request, "admin/all_reviews.html", context={"active": "reviews"})
+    return render(request, "dashboard/all_reviews.html", context={"active": "reviews"})
+
+
+# * Business Views
+
+
+class BusinessViewSetup:
+    model = Business
+    success_url = reverse_lazy("dashboard_business_list")
+
+
+class BusinessListView(BusinessViewSetup, ListView):
+    context_object_name: Optional[str] = "businesses"
+    template_name: str = "dashboard/business_list.html"
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["active"] = "businesses"
+        return context
+
+
+class BusinessCreateView(BusinessViewSetup, CreateView):
+    fields = ["name", "owner", "operation_commenced"]
+    template_name: str = "dashboard/business_form.html"
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["active"] = "businesses"
+        return context
+
+
+class BusinessUpdateView(BusinessViewSetup, UpdateView):
+    fields = ["name", "owner", "operation_commenced"]
+    template_name: str = "dashboard/business_update_form.html"
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["active"] = "businesses"
+        return context
+
+
+class BusinessDetailView(BusinessViewSetup, DetailView):
+    context_object_name: Optional[str] = "business"
+    template_name: str = "dashboard/business_detail.html"
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["active"] = "businesses"
+        return context
+
+
+def delete_business(request, pk):
+    business = get_object_or_404(Business, pk=pk)
+    business.delete()
+    return redirect(reverse("dashboard_business_list"))
+
+
+class BoatViewSetup:
+    model = Boat
+    success_url = reverse_lazy("dashboard_boat_list")
+
+
+class BoatListView(BoatViewSetup, ListView):
+    context_object_name: Optional[str] = "boats"
+    template_name: str = "dashboard/boat_list.html"
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["active"] = "boats"
+        return context
+
+
+class BoatCreateView(BoatViewSetup, CreateView):
+    fields = [
+        "name",
+        "capacity",
+        "captain_name",
+        "captain_photo",
+        "deckhand",
+        "business",
+    ]
+    template_name: str = "dashboard/boat_form.html"
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["active"] = "boats"
+        return context
+
+
+class BoatDetailView(BoatViewSetup, DetailView):
+    context_object_name: Optional[str] = "boat"
+    template_name: str = "dashboard/boat_detail.html"
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["active"] = "boats"
+        return context
+
+
+# FIXME fix form
+class BoatUpdateView(BoatViewSetup, UpdateView):
+    fields = [
+        "name",
+        "capacity",
+        "captain_name",
+        "captain_photo",
+        "deckhand",
+        "business",
+    ]
+
+    template_name: str = "dashboard/boat_update_form.html"
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["active"] = "businesses"
+        return context
+
+
+def delete_business(request, pk):
+    boat = get_object_or_404(Boat, pk=pk)
+    boat.delete()
+    return redirect(reverse("dashboard_boat_list"))
